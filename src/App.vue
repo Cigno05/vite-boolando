6d0f3efdc94422cbe50f8984ce894a44e65385d8
Cigno05 <paolocalcagni.82@gmail.com>
2024-04-10 14:48:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<!-- JS -->
+<script>
+//import
+//import
+
+export default {
+
+data() {
+    return {
+
+	}
+  },
+  watch: {
+	
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<!-- HTML -->
+
+<template>
+  <h1>Ciao</h1>
+</template>
+
+<!-- CSS -->
+
+<style lang="scss">
+
+</style>
